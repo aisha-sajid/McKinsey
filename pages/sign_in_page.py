e13f437e78e0a1efaab10f88ef7b5e35780e174f
aisha-sajid <aisha.sajid198@gmail.com>
2017-07-21 17:29:03 +0500

    changed the sign_in_page.py

--- pages/sign_in_page.py
+++ pages/sign_in_page.py
@@ -1,2 +1,34 @@
+from common.utils import USERNAME, PASSWORD
+from bok_choy.page_object import PageObject
+
 
 class SignInPage(PageObject):
+    """
+    Sign In page
+    """
+
+    url = None
+
+    def is_browser_on_page(self):
+        return self.q(css="").visible
+
+    def enter_login_username(self):
+        """
+        fill username field with user's username
+        :return:
+        """
+        self.q(css="").fill(USERNAME)
+
+    def enter_login_password(self):
+        """
+        fill password field with password
+        :return:
+        """
+        self.q(css="").fill(PASSWORD)
+
+    def click_login_button(self):
+        """
+        Click login button
+        :return:
+        """
+        self.q(css="").click()
